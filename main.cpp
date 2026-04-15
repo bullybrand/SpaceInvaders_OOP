c86b17de1a5b18b55554534b08693fc44f131fba
Ayaan <ayaanshoaib07@gmail.com>
2026-04-16 02:09:20 +0500

    cleaned up github, fixed everything

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
-#include <iostream>
-using namespace std;
+#include "raylib.h"
 
-int main(){
-    cout<<"testing";
+int main() {
+    InitWindow(800, 450, "Raylib Working");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("It works!", 300, 200, 20, DARKBLUE);
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
 }
