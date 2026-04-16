7629bec2911312d317e987c4d472023da2d6c686
Ayaan <ayaanshoaib07@gmail.com>
2026-04-16 13:36:55 +0500

    changes in json

--- main.cpp
+++ main.cpp
@@ -2,10 +2,6 @@
 #include "player.h"
 
 int main(){
-<<<<<<< HEAD
-=======
-    InitWindow(1800,1600,"space thing");
->>>>>>> cfa1902fd36ac081e3189376f7c4fe5282e50a08
 
     const int swidth = 1400;
     const int sheight =1000;
